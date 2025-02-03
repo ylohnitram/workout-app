3a8b3b8831342710558d9fcaf7e8fa4e7498e86f
Martin Holy <mholy1983@protonmail.com>
2025-02-03 18:56:35 +0100

    fix db issue

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -16,15 +16,14 @@ if (!apps.length) {
     throw new Error('FIREBASE_PRIVATE_KEY is not set in environment variables');
   }
 
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY.startsWith('-----')
-    ? process.env.FIREBASE_PRIVATE_KEY
-    : Buffer.from(process.env.FIREBASE_PRIVATE_KEY, 'base64').toString('ascii');
+  // Převedeme \n na skutečné nové řádky
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
 
   initializeApp({
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: privateKey,
+      privateKey,
     }),
   });
 }
